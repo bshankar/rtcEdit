b8c9360f89fbfc9c8b5c5b672463e83ba910924b
Bhavani Shankar <ebhavanishankar@gmail.com>
2018-03-07 11:59:51 +0530

    Move the callbacks for join, leave and message to util.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const uuidv4 = require('uuid/v4')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-const { getUsers, findSocket } = require('./util')
+const { onJoin, onLeave, onMessage } = require('./util')
 
 app.use(express.static('public'))
 app.use(bodyParser.json())
@@ -25,26 +25,9 @@ app.get('/:docId', function (req, res) {
 })
 
 io.on('connection', function (socket) {
-  console.log(socket.id + ' connected')
-
-  socket.on('join room', data => {
-    socket.join(data.docId)
-    io.to(data.docId).emit('user joined', getUsers(data.docId, io))
-  })
-
-  socket.on('leave room', data => {
-    socket.leave(data.docId)
-    io.to(data.docId).emit('user left', getUsers(data.docId, io))
-  })
-
-  socket.on('message', data => {
-    const _data = {from: socket.id, ...data}
-    if (data.to) {
-      findSocket(data.to).emit('message', _data)
-    } else {
-      socket.broadcast.to(data.docId).emit('message', _data)
-    }
-  })
+  socket.on('join room', data => onJoin(data, socket, io))
+  socket.on('leave room', data => onLeave(data, socket, io))
+  socket.on('message', data => onMessage(data, socket, io))
 })
 
 server.listen(port, function () {
--- util.js
+++ util.js
@@ -11,4 +11,23 @@ function findSocket (id, room, io) {
   return _room[id]
 }
 
-module.exports = { getUsers, findSocket }
+function onJoin (data, socket, io) {
+  socket.join(data.docId)
+  io.to(data.docId).emit('user joined', getUsers(data.docId, io))
+}
+
+function onLeave (data, socket, io) {
+  socket.leave(data.docId)
+  io.to(data.docId).emit('user left', getUsers(data.docId, io))
+}
+
+function onMessage (data, socket) {
+  const _data = {from: socket.id, ...data}
+  if (data.to) {
+    findSocket(data.to).emit('message', _data)
+  } else {
+    socket.broadcast.to(data.docId).emit('message', _data)
+  }
+}
+
+module.exports = { onJoin, onLeave, onMessage }
